50198223917d6e2a7175187ee16f5dda89e4458e
Manuel Limones <limonesu.me@gmail.com>
2015-09-12 01:39:46 -0500

    Add skeleton header window_manager.h

--- include/window_manager.h
+++ include/window_manager.h
@@ -1,5 +1,23 @@
-#ifndef WINDOW_MANAGER_H
-#define WINDOW_MANAGER_H
+/* 
+ * File:   window_manager.h
+ * Author: eLimones <limonesu.me@gmail.com>
+ *
+ * Created on September 12, 2015, 1:38 AM
+ */
 
+#ifndef WINDOW_MANAGER_H
+#define	WINDOW_MANAGER_H
+
+#ifdef	__cplusplus
+extern "C" {
 #endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* WINDOW_MANAGER_H */
 
